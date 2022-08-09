4bb1b72a2e50782ab13f698f6149de047e956683
MariaMantusova <maria.mantusova@mail.ru>
2022-08-10 00:45:42 +0300

    fix: удалить лишний контейнер, добавить type для button

--- scripts/index.js
+++ scripts/index.js
@@ -2,10 +2,11 @@ let main = document.querySelector('.main')
 let editButton = main.querySelector('.profile__edit-button');
 let popup = document.querySelector('.popup');
 let closeButton = popup.querySelector('.popup__button-close');
-let profileInfo = main.querySelector('.profile__info')
 let saveButton = popup.querySelector('.popup__button');
-
-popup.classList.remove('popup_opened');
+let profileName = popup.querySelector('.popup__item_el_name');
+let profileJob = popup.querySelector('.popup__item_el_job');
+let profileNameField = main.querySelector('.profile__name');
+let profileJobField = main.querySelector('.profile__description');
 
 function openPopup() {
     popup.classList.add('popup_opened');
@@ -15,24 +16,15 @@ function closePopup() {
     popup.classList.remove('popup_opened');
 }
 
-editButton.addEventListener('click', openPopup);
-closeButton.addEventListener('click', closePopup);
-
 function changeInfo() {
-    let profileName = popup.querySelector('.popup__item_el_name');
-    let profileJob = popup.querySelector('.popup__item_el_job');
-    let profileNameField = main.querySelector('.profile__name');
-    let profileJobField = main.querySelector('.profile__description');
-
     profileNameField.innerHTML = profileName.value;
     profileJobField.innerHTML = profileJob.value;
 
     closePopup();
-
-    profileName.value = '';
-    profileJob.value = '';
 }
 
+editButton.addEventListener('click', openPopup);
+closeButton.addEventListener('click', closePopup);
 saveButton.addEventListener('click', changeInfo);
 
 
