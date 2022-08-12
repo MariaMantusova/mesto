3938df614c9f74aba31da4d2ee7826a730c0533a
MariaMantusova <maria.mantusova@mail.ru>
2022-08-12 14:42:44 +0300

    fix: выводить в попап значение из html

--- scripts/index.js
+++ scripts/index.js
@@ -2,15 +2,16 @@ let main = document.querySelector('.main')
 let editButton = main.querySelector('.profile__edit-button');
 let popup = document.querySelector('.popup');
 let closeButton = popup.querySelector('.popup__button-close');
-let saveButton = popup.querySelector('.popup__button');
 let profileName = popup.querySelector('.popup__item_el_name');
 let profileJob = popup.querySelector('.popup__item_el_job');
 let profileNameField = main.querySelector('.profile__name');
 let profileJobField = main.querySelector('.profile__description');
-let form = popup.querySelector('.popup__form')
+let form = popup.querySelector('.popup__form');
 
 function openPopup() {
     popup.classList.add('popup_opened');
+    profileName.value = profileNameField.textContent;
+    profileJob.value =profileJobField.textContent;
 }
 
 function closePopup() {
