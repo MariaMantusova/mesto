79a7ad0aa9245f988669eed66b57604664190824
MariaMantusova <maria.mantusova@mail.ru>
2022-10-27 11:26:11 +0300

    fix: fix adding card

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import './index.css';
 import Card from '../components/Card.js'
-import {initialCards} from '../utils/cards.js'
 import FormValidator from '../components/FormValidator.js'
 import Section from '../components/Section.js'
 import PopupWithImage from '../components/PopupWithImage.js';
